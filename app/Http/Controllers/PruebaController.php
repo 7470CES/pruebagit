b58f217109147b99e313e27f66bc78c7f16b2b23
*7470ces* <*elbertjosesalasbrochero@gmail.com*>
2021-06-02 16:38:18 -0500

    he creado vista,controlador, y ruta

---
+++ app/Http/Controllers/PruebaController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PruebaController extends Controller
+{
+    //
+
+
+
+    public function cargar_vista(){
+
+
+
+        return view('master');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PruebaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get("/prueba", [PruebaController::class, "cargar_vista"])->name('cargar_vista');
